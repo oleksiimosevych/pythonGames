af803611da1a7c015648c64f553103af227d882c
oleksiimosevych <oleksii.mosevych@gmail.com>
2019-09-03 19:12:14 +0300

    I need to change it a little bit

--- MY1.py
+++ MY1.py
@@ -1,12 +1,11 @@
 #first programm in Py
-#ARE YOU FAMOUS? TALK SHOW v.1.3
+#ARE YOU FAMOUS? TALK SHOW v.2.0
 
-famousProgrammers="Durov, Mosevych, Zukkerberg, Cuccerberg"
-
+famousProgrammers=['Durov', 'Mosevych', 'Zukkerberg', 'Cuccerberg']
 famousPersonsM=['Jobs','Musk']
 famousPersonsW=['Opra Winfrie','Jessica Alba', 'Jennifer Aniston', 'Avril Lavigne']
 
-puilo="Putin, Putyn, Puten, putin, putyn, pu, puten"
+puilo=['Putin', 'Putyn', 'Puten', 'putin', 'putyn', 'pu', 'puten']
 
 while(1==1): #break need
     #declaration of variables
@@ -23,6 +22,8 @@ while(1==1): #break need
     onceMore="Oh Wow! And what happen later?"
 
     #I will push it into function later
+    #I want to create another file for this... is it possible?)
+    
     if(timeNow in morningTime):
         time = 'Good morning! '
     elif(timeNow in dayTime):
@@ -36,28 +37,35 @@ while(1==1): #break need
         break
         
     #the game
-    print("Now we know the time! It is time for our Talk Show! Are you famous not? \n Lets start it! \n")
+    print("Now we know the time! It is time for our Talk Show! Are you famous or not? \n Lets start it! \n")
     name = input('What is your Surname? \n')
-    if (name in famousProgrammers):
-        print('Hello ', name, ' The greatest programmer of the world!\n Tell us your story')
-        story+=input()
         
     i=0
     print(time , ' You are so famous!  \nPlease tell us about your life. \n \nIf you want to end the story you must write <The End>')
         
         
     while(i<99):
-        #only to test for with no braackets
-        for name in famousPersonsM:
-            print('And what else can you tell us Mr. ', name, '? ')
+        if name == famousProgrammers[i]:
+            print('Hello ', name, ' The greatest programmer of the world!\n Tell us your story')
+            #REPLACE.. this must be a callable function.. else too much code.
             story+=input()
-            
             if (end in story)or(nothing in story):
                 break
             else:
                 print(onceMore)
                 story+= input()
-        for name in famousPersonsW:
+            #until here
+                
+        elif name==famousPersonsM[i]:
+            print('And what else can you tell us Mr. ', name, '? ')
+            story+=input()
+            
+            if end in story or nothing in story:
+                break
+            else:
+                print(onceMore)
+                story+= input()
+        elif(name==famousPersonsW[i]):
             print('And what else can you tell us Mrs. ', name, '? Have I already told you how goodlooking you are today? So tell us more! ')
             story=input()
             if (end in story):
@@ -66,24 +74,23 @@ while(1==1): #break need
             else:
                 print('It is very interesting, Mrs. ', name, '! So tell me more! ')
                 story=input()
-    if(name in puilo):
-        print("Hell to you!")
-        a=1
-        while (a<10):
-            print("PUTIN HUILO! LALA LLALA LAALA LALA!")
-            a=a+1
-            #playThatMusic
-            #open youTube video agains pu)
+        elif name == puilo[i]:
+            print("Hell to you!")
+            a=1
+            while (a<10):#9 times
+                print("PUTIN HUILO! LALA LLALA LAALA LALA!")
+                a=a+1
+                #playThatMusic
+                #open youTube video agains pu)
+
     ending=input("If you want to know all the story type WHOLE STORY using UPPERCASE symbols. Thank you.")
-    if(ending=='WHOLE STORY'):
+    if ending=='WHOLE STORY':
         print(story)
         break
     elif(name=="exit"):
-            break
+        break
     else:
-        print('Who are you, Mr. '+name+'? This is the end of the game. Try again later!')
- 
+        print('Who are you, Mr. '+name+'? This is the end of the game. \nTry again later!')
+        break
 
-                
-        
-    
+            
