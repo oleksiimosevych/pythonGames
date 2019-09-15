fc69ef97f9d22bc447cf22965b3df4969fcbfebf
oleksiimosevych <oleksii.mosevych@gmail.com>
2019-09-15 14:38:55 +0300

    created and added things to admin panel

--- datebank/certification82/admin.py
+++ datebank/certification82/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Document, Project, EzeHersteller, EzeTyp, EzeNeu, EzeBestand
 # Register your models here.
+
+admin.site.register(Document)
+admin.site.register(Project)
+admin.site.register(EzeHersteller)
+admin.site.register(EzeTyp)
+admin.site.register(EzeNeu)
+admin.site.register(EzeBestand)
