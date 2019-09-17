97566b16a26374fc19a23463be9d6cc4af484e2b
oleksiimosevych <oleksii.mosevych@gmail.com>
2019-09-17 19:21:35 +0300

    page 404 defined new

--- datebank/certification82/views.py
+++ datebank/certification82/views.py
@@ -1,4 +1,7 @@
+#rendering possible
 from django.shortcuts import render
+#add 404
+from django.http import Http404
 from django.http import HttpResponse
 from .models import EzeNeu, EzeBestand, Project
 from django.template import loader
@@ -11,14 +14,19 @@ def index(request):
 	# project_Names = ' , '.join([p.project_name for p in project_name])
 	#added template
 	template = loader.get_template('certification82/index.html')
-	context = {
-		'project_name_list': project_name_list,
-	}
-	return HttpResponse(template.render(context, request))
+	context = {	'project_name_list': project_name_list, }
+	return render(request, 'certification82/index.html', context)
 	# return HttpResponse(project_Names+'\n\n<br> Neue Ezes: \n'+eze_Neus )
 #add more views
 def detail(request, project_id):
-	return HttpResponse("You're looking at Project %s." % project_id)
+	try:
+		latest_EzeNeu_list = EzeNeu.objects.get(pk=project_id)
+	except EzeNeu.DoesNotExist:
+		raise Http404("No Eze with this project number: "+str(project_id))
+	
+	context = {	'latest_EzeNeu_list': latest_EzeNeu_list, }
+	
+	return render(request, 'certification82/details.html', {'latest_EzeNeu_list' : latest_EzeNeu_list})
 
 def results(request, project_id):
 	response = "You're looking at the results of project %s."
@@ -32,7 +40,7 @@ def ezenues(request):
 	latest_EzeNeu_list = EzeNeu.objects.order_by('-vDE_EZE1_Name')[:3]
 	eze_Neus = ', '.join([e.vDE_EZE1_Name for e in latest_EzeNeu_list])
 	# project_Names = ', '.join([p.project_name for p in project_name])
-	return HttpResponse('<br> Neue Ezes: \n'+eze_Neus )
+	return HttpResponse('<br> <h2>Neue Ezes:</h2> <br>'+eze_Neus )
 
 def ezebestands(request):
 	# project_name = Project.objects.order_by('-project_name')[:3]
