d69da3cae23095dd683f9d51f86d1dd82446dd43
oleksiimosevych <oleksii.mosevych@gmail.com>
2019-10-02 13:55:23 +0300

    productionv1

--- datebank/datebank/settings.py
+++ datebank/datebank/settings.py
@@ -89,6 +89,11 @@ DATABASES = {
 }
 
 
+try:
+    from .settings_prod import *
+except:
+    pass
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
---
+++ datebank/datebank/settings_prod.py
@@ -0,0 +1,16 @@
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        #for SQLITE3
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #for MYSQL
+        # 'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db1',
+        'USER': 'certification82',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
