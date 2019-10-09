9b55292efb53b7529f461556776e26af7d61dd21
oleksiimosevych <oleksii.mosevych@gmail.com>
2019-10-09 12:24:31 +0300

    tested

--- datebank/datebank/settings_prod.py
+++ datebank/datebank/settings_prod.py
@@ -5,5 +5,10 @@ DATABASES = {
     'default': {
         #for SQLITE3
         # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #for MYSQL
+        # 'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
